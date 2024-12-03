b61c086ff2a27609cbf59d526b2ee29cb5bd1552
Phi Ton <pton@uw.edu>
2024-12-03 12:16:38 -0800

    Day 3

--- days/day_03/main.go
+++ days/day_03/main.go
@@ -2,6 +2,9 @@ package day_03
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // Run function of the daily challenge
@@ -16,10 +19,47 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	result := 0
+	instructions := strings.Join(input, "")
+
+	mulRegex, _ := regexp.Compile("mul\\(([0-9]+),([0-9]+)\\)")
+
+	matches := mulRegex.FindAllStringSubmatch(instructions, -1)
+
+	for _, match := range matches {
+		num1, _ := strconv.Atoi(match[1])
+		num2, _ := strconv.Atoi(match[2])
+		fmt.Println(match)
+		result += num1 * num2
+	}
+
+	fmt.Println()
+
+	return strconv.Itoa(result)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	result := 0
+	enabled := true
+	instructions := strings.Join(input, "")
+
+	mulRegex, _ := regexp.Compile("mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)")
+
+	matches := mulRegex.FindAllStringSubmatch(instructions, -1)
+
+	for _, match := range matches {
+		if match[0] == "do()" {
+			enabled = true
+		} else if match[0] == "don't()" {
+			enabled = false
+		} else if enabled {
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+			fmt.Println(match)
+			result += num1 * num2
+		}
+	}
+
+	return strconv.Itoa(result)
 }
